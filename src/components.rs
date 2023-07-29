8056c5659327203e4bff0adf3e685e91a88dba08
chojs23 <chojs990222@gmail.com>
2023-07-29 18:12:59 +0900

    Add random movement and relevant component
    Added a new system 'random_move' that changes entities' positioning randomly in a grid map. This system writes Position and reads the new component introduced 'MovingRandomly'. This component has been added to the enemy spawner, allowing enemies to move randomly around the map. The random_move system is included in the scheduler to ensure its execution.

--- src/components.rs
+++ src/components.rs
@@ -11,3 +11,6 @@ pub struct Player; // Tag Component
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy; // Tag Component
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct MovingRandomly;
